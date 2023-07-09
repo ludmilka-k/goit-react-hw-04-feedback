37d6bb26b05f56f095f3c15f93594224199b082b
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-07-09 19:50:10 +0300

    Add icons

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,22 +1,58 @@
 import PropTypes from 'prop-types';
+import { TbMoodSmile, TbMoodEmpty, TbMoodSad } from "react-icons/tb";
 import css from './FeedbackOptions.module.css';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-	return (
+  return (
     <>
-      <div className={css.wrapper}>
-        {options.map(option => {
-          return (
-            <button
-              key={option}
-              type="button"
-              className={css.button}
-              onClick={() => onLeaveFeedback(option)}
-            >
-              {option}</button>
-          )
-        }
-      )}
-      </div>
+      {/*<ul className={css.wrapper}>*/}
+      {/*  {options.map(option => (*/}
+      {/*    <li key={option}>*/}
+      {/*      <button*/}
+      {/*        name={option}*/}
+      {/*        type="button"*/}
+      {/*        className={css.button}*/}
+      {/*        onClick={onLeaveFeedback}*/}
+      {/*      >*/}
+      {/*        {option}</button>*/}
+      {/*    </li>*/}
+      {/*    ))}*/}
+      {/*</ul>*/}
+
+        <ul className={css.wrapper}>
+          {options.map(option => (
+            <li key={option}>
+              {option === 'good' && (
+              <button
+                name={option}
+                type="button"
+                className={css.button}
+                onClick={onLeaveFeedback}
+              >
+                <TbMoodSmile className={css.iconGood} size="20"/>{option}</button>
+              )}
+
+              {option === 'neutral' && (
+                <button
+                  name={option}
+                  type="button"
+                  className={css.button}
+                  onClick={onLeaveFeedback}
+                >
+                  <TbMoodEmpty className={css.iconNeutral} size="20"/>{option}</button>
+              )}
+
+              {option === 'bad' && (
+                <button
+                  name={option}
+                  type="button"
+                  className={css.button}
+                  onClick={onLeaveFeedback}
+                >
+                  <TbMoodSad className={css.iconBad} size="20"/>{option}</button>
+              )}
+            </li>
+          ))}
+        </ul>
     </>
 	)
 }
