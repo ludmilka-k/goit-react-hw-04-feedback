b722b4e74e6caf650d9c23ee982629269e3da1b4
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-07-11 17:33:42 +0300

    update FeedbackOptions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,30 @@
-
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { FeedbackStatistics } from './FeedbackStatistics/FeedbackStatistics';
-import {Notification} from './Notification/Notification';
+import { Notification } from './Notification/Notification';
+import { TbMoodEmpty, TbMoodSad, TbMoodSmile } from 'react-icons/tb';
+import css from './FeedbackOptions/FeedbackOptions.module.css';
 
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-    const handleFeedback= (event) => {
-      const btnType= event.target.name;
-      if (btnType === "good") setGood(good + 1);
-      if (btnType === "neutral") setNeutral(neutral + 1);
-      if (btnType === "bad") setBad(bad + 1);
+  const handleFeedback = (event) => {
+    const btnType = event.target.name;
+    switch (btnType) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
     }
+  };
 
   const countTotalFeedback = () => {
     return good + neutral + bad;
@@ -26,25 +35,31 @@ export const App = () => {
     return Math.round((good / total) * 100);
   };
 
+  const options = {
+    good: <TbMoodSmile className={css.iconGood} size='20' />,
+    neutral: <TbMoodEmpty className={css.iconNeutral} size='20' />,
+    bad: <TbMoodSad className={css.iconBad} size='20' />,
+  };
+
   return (
     <>
-      <Section title={"Please leave feedback"}>
+      <Section title={'Please leave feedback'}>
         <FeedbackOptions
-          options={Object.keys({ good, neutral, bad })}
+          options={options}
           onLeaveFeedback={handleFeedback}
         />
       </Section>
-      <Section title={"Statistics"}>
+      <Section title={'Statistics'}>
         {countTotalFeedback() ? (
           <FeedbackStatistics
             good={good}
             neutral={neutral}
             bad={bad}
             total={countTotalFeedback()}
-            positivePercentage={ countPositiveFeedbackPercentage()} />
-          ) : ( <Notification message="There is no feedback"/>)
+            positivePercentage={countPositiveFeedbackPercentage()} />
+        ) : (<Notification message='There is no feedback' />)
         }
       </Section>
     </>
-  )
-}
+  );
+};
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,61 +1,27 @@
 import PropTypes from 'prop-types';
-import { TbMoodSmile, TbMoodEmpty, TbMoodSad } from "react-icons/tb";
 import css from './FeedbackOptions.module.css';
+
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <>
-      {/*<ul className={css.wrapper}>*/}
-      {/*  {options.map(option => (*/}
-      {/*    <li key={option}>*/}
-      {/*      <button*/}
-      {/*        name={option}*/}
-      {/*        type="button"*/}
-      {/*        className={css.button}*/}
-      {/*        onClick={onLeaveFeedback}*/}
-      {/*      >*/}
-      {/*        {option}</button>*/}
-      {/*    </li>*/}
-      {/*    ))}*/}
-      {/*</ul>*/}
-
-        <ul className={css.wrapper}>
-          {options.map(option => (
-            <li key={option}>
-              {option === 'good' && (
+      <ul className={css.wrapper}>
+        {Object.entries(options)
+          .map(([optionName, optionIcon]) => (
+            <li key={optionName}>
               <button
-                name={option}
-                type="button"
+                name={optionName}
+                type='button'
                 className={css.button}
                 onClick={onLeaveFeedback}
               >
-                <TbMoodSmile className={css.iconGood} size="20"/>{option}</button>
-              )}
-
-              {option === 'neutral' && (
-                <button
-                  name={option}
-                  type="button"
-                  className={css.button}
-                  onClick={onLeaveFeedback}
-                >
-                  <TbMoodEmpty className={css.iconNeutral} size="20"/>{option}</button>
-              )}
-
-              {option === 'bad' && (
-                <button
-                  name={option}
-                  type="button"
-                  className={css.button}
-                  onClick={onLeaveFeedback}
-                >
-                  <TbMoodSad className={css.iconBad} size="20"/>{option}</button>
-              )}
+                {optionIcon} {optionName}
+              </button>
             </li>
           ))}
-        </ul>
+      </ul>
     </>
-	)
-}
+  );
+};
 
 FeedbackOptions.prototype = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
